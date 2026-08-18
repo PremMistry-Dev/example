ddbe7a18cd6e087219cac7bf28c768b2c6e44a9c
Pops <mistryprem15@gmail.com>
2026-08-18 22:43:40 +0530

    layout changes

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -4,15 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Home</title>
+        <title>My Website</title>
 
      
     </head>
     <body>
-   
-      <x-nav></x-nav>
+   <nav>
+      <x-nav-link href="/">Home</x-nav-link>
+      <x-nav-link href="/about">About</x-nav-link>
+      <x-nav-link href="/contact">Contact</x-nav-link>
       <!-- <h1>Hi poppsy!!</h1> -->
-   
+   </nav>
       {{ $slot }}
     </body>
 </html>
